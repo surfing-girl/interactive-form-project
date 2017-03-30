52a76048153a284cf6a30ef63617b12bd5b71afb
surfing-girl <joanna.demecka@gmail.com>
2017-03-30 17:57:16 +0100

    Form validation

--- js/app.js
+++ js/app.js
@@ -2,92 +2,89 @@
 
 //1. Set focus on the first field-->autofocus?
 
-
 /* -----JOB ROLE SECTION----- */
 
 /* Function selects elements */
 
-function OtherJobRole() {
+function OtherJobRoleInit() {
 	this.roleSelectElement = document.getElementById('title');
-	this.other_title = document.getElementById('other-title');
+	$('#other-title').hide();
+	$('#other-title').prev().hide();
 }
 
 /* "Your job role" text field appears when user selects "Other" from the Job Role menu */
 
-OtherJobRole.prototype.otherJobRoleInputRevealing = function () {
+OtherJobRoleInit.prototype.otherJobRoleInputRevealing = function () {
 	this.roleSelectElement.addEventListener('change', (e) => {
 		if(this.roleSelectElement.value === 'other') {
-			$(this.other_title).toggle('slow');
+			$('#other-title').show('slow');
+			$('#other-title').prev().prev().show('slow');
 		} else {
-			$(this.other_title).hide('slow');
+			$('#other-title').hide('slow');
+			$('#other-title').prev().prev().hide('slow');
 		};
 	})
 };
 
 /* Making instation of OtherJobRole class and calling its method */
-const otherJobSelector = new OtherJobRole();
+const otherJobSelector = new OtherJobRoleInit();
 otherJobSelector.otherJobRoleInputRevealing();
 
-//3. --T-shirt INFO SECTION--
-	//a) for the t-shirt color menu, only display the options that match the design selected in the menu
-		//--> if 'Theme-js Puns' then color menu should only display it's colors
 
-// function tShirtSelection() {
-//
-// }
+/* -----T-shirt INFO SECTION----- */
 
-var tshirtDesignSelect = document.getElementById('design');
-var tshirtColorMenu = document.getElementById('color');
-var tshirtOptionsList = tshirtColorMenu.children;
-var designsList = tshirtDesignSelect.children;
+function TShirtInfoInit() {
+	this.tshirtDesignSelect = document.getElementById('design');
+	this.tshirtColorMenuDiv = document.getElementById('colors-js-puns')
+	this.tshirtColorMenu = document.getElementById('color');
+	this.tshirtColorOptionsList = this.tshirtColorMenu.children;
+	this.designsList = this.tshirtDesignSelect.children;
+	this.pattern = /JS Puns/;
+	this.pattern2 = /I ♥/;
+	this.tshirtColorMenuDiv.style.display = 'none';
+}
 
-for (var i = 0; i < tshirtOptionsList.length; i++) {
-	tshirtOptionsList[i].style.display = 'none';
-};
+/* Function shows T-shirt color options based on the design selected. */
 
-tshirtDesignSelect.onchange = function () {
-	tshirtColorMenu.disabled = false;
-	for (var i = 0; i < tshirtOptionsList.length; i++) {
-		if(tshirtOptionsList[i].value == 'choose-design') {
-			tshirtColorMenu.removeChild(tshirtOptionsList[i]);
-		}
-	};
-	for (var i = 0; i < designsList.length; i++) {
-		if(designsList[i].value == 'select theme') {
-			tshirtDesignSelect.removeChild(designsList[i]);
-		}
-	};
-
-	var pattern = /JS Puns/;
-	var pattern2 = /I ♥/;
-
-	for (var i = 0; i < tshirtOptionsList.length; i++) {
-		var colorInner = tshirtOptionsList[i].innerText;
-		if(tshirtDesignSelect.value === 'js puns') {
-			tshirtOptionsList[1].selected = true;
-			if (pattern.test(colorInner)) {
-				tshirtOptionsList[i].style.display = 'block';
+TShirtInfoInit.prototype.showingSelectedThemeColors = function () {
+	this.tshirtColorMenuDiv.style.display = 'block';
+	for (let i = 0; i < this.tshirtColorOptionsList.length; i++) {
+		const colorInner = this.tshirtColorOptionsList[i].innerText;
+		if(this.tshirtDesignSelect.value === 'js puns') {
+			this.tshirtColorOptionsList[1].selected = true;
+			if (this.pattern.test(colorInner)) {
+				this.tshirtColorOptionsList[i].style.display = 'block';
 			} else {
-				tshirtOptionsList[i].style.display = 'none';
+				this.tshirtColorOptionsList[i].style.display = 'none';
 			};
-		} else if(tshirtDesignSelect.value === 'heart js') {
-			tshirtOptionsList[3].selected = true;
-			if (pattern2.test(colorInner)) {
-				tshirtOptionsList[i].style.display = 'block';
-
+		} else if(this.tshirtDesignSelect.value === 'heart js') {
+			this.tshirtColorOptionsList[3].selected = true;
+			if (this.pattern2.test(colorInner)) {
+				this.tshirtColorOptionsList[i].style.display = 'block';
 			} else {
-				tshirtOptionsList[i].style.display = 'none';
+				this.tshirtColorOptionsList[i].style.display = 'none';
 			};
 		};
 	};
+}
+
+TShirtInfoInit.prototype.addOnChangeTShirtFormHandler = function () {
+	this.tshirtDesignSelect.addEventListener('change', (e) => {
+		tShirtInfoInit.showingSelectedThemeColors();
+	});
 };
 
-//4. --REGISTER FOR ACTIVITIES SECTION--
-	//a) don't allow to select an event which is at the same time as selected before
+const tShirtInfoInit = new TShirtInfoInit();
+tShirtInfoInit.addOnChangeTShirtFormHandler();
 
 
-function ActivitiesRegistration () {
-	var activitiesListElements = document.getElementsByClassName('activities')[0].children;
+/* -----REGISTER FOR ACTIVITIES SECTION----- */
+
+function ActivitiesRegistrationInit() {
+	this.totalElement = document.createElement('p');
+	this.totalElement.setAttribute("id", "total");
+	document.getElementsByClassName('activities')[0].appendChild(this.totalElement);
+	const activitiesListElements = document.getElementsByClassName('activities')[0].children;
 	this.activitiesList = [];
 	this.datePattern = /(\w+)\s[\d+](\w+)-(\d+)(\w+)/;
 	this.pricePattern = /\$(\d+)/;
@@ -95,46 +92,53 @@ function ActivitiesRegistration () {
 	this.price;
 	this.totalPrice = 0;
 
-	for (var i = 0; i < activitiesListElements.length; i++) {
+	for (let i = 0; i < activitiesListElements.length; i++) {
 		this.activitiesList.push(activitiesListElements[i]);
 	}
 }
 
-ActivitiesRegistration.prototype.sameDateCheckboxDisabled = function(element) {
+/* Function disables checkboxes which has conflicting dates */
+
+ActivitiesRegistrationInit.prototype.sameDateCheckboxDisabled = function(element) {
 	for (let i = 0; i < this.activitiesList.length; i++) {
 		if(this.datePattern.test(this.activitiesList[i].innerText)) {
-			var otherDate = this.activitiesList[i].innerText.match(this.datePattern)[0];
+			const otherDate = this.activitiesList[i].innerText.match(this.datePattern)[0];
 			if (this.date === otherDate && this.activitiesList.indexOf(element) != this.activitiesList.indexOf(this.activitiesList[i])) {
 				this.activitiesList[i].firstChild.disabled = true;
+				this.activitiesList[i].style.color = 'grey';
 			};
 		};
 
 	};
 };
 
-ActivitiesRegistration.prototype.checkboxesEnabled = function(element) {
-	for (var i = 0; i < this.activitiesList.length; i++) {
+/* Function enables checkboxes when conflicted date was unchecked */
+
+ActivitiesRegistrationInit.prototype.checkboxesEnabled = function(element) {
+	for (let i = 0; i < this.activitiesList.length; i++) {
 		if(this.datePattern.test(this.activitiesList[i].innerText)) {
-			var otherDate = this.activitiesList[i].innerText.match(this.datePattern)[0];
+			const otherDate = this.activitiesList[i].innerText.match(this.datePattern)[0];
 			if (this.date === otherDate) {
 				this.activitiesList[i].firstChild.disabled = false;
+				this.activitiesList[i].style.color = 'black';
 			};
 		};
 
 	};
 };
 
-ActivitiesRegistration.prototype.showTotalPrice = function(message) {
-	var totalPriceContainer = document.getElementById('total');
+/* Function prints total price */
+
+ActivitiesRegistrationInit.prototype.showTotalPrice = function(message) {
+	const totalPriceContainer = document.getElementById('total');
 	totalPriceContainer.innerText = message;
 };
 
-var activitiesRegistrationForm = new ActivitiesRegistration();
-
+var activitiesRegistrationForm = new ActivitiesRegistrationInit();
 
 activitiesRegistrationForm.activitiesList.forEach(function (element) {
-	var input = element.firstChild;
-	element.onchange = function () {
+	const input = element.firstChild;
+	element.addEventListener('change', (e) => {
 		if(activitiesRegistrationForm.datePattern.test(element.innerText)){
 			activitiesRegistrationForm.date = element.innerText.match(activitiesRegistrationForm.datePattern)[0];
 		} else {
@@ -155,53 +159,44 @@ activitiesRegistrationForm.activitiesList.forEach(function (element) {
 			activitiesRegistrationForm.totalPrice -= activitiesRegistrationForm.price;
 			activitiesRegistrationForm.showTotalPrice('Total: ' + activitiesRegistrationForm.totalPrice);
 		};
-	};
+	});
 });
 
-//5. --PAYMENT INFO SECTION--
+/* -----PAYMENT INFO SECTION----- */
+
 function PaymentInfo () {
 	this.paymentInfoMenu = document.getElementById('payment');
 	this.creditCardDiv = document.getElementById('credit-card');
-	this.selectOptions = [];
-	this.menuChildren = this.paymentInfoMenu.children;
-	$('#payment').next().hide();
-	$('.info').hide();
-	$('#credit-card').hide();
-	for (var i = 0; i < this.menuChildren.length; i++) {
-		this.selectOptions.push(this.menuChildren[i]);
-	};
+	this.paypalInfo = $('#credit-card').next();
+	this.paypalInfo.hide();
+	this.bitCoinInfo = $('#credit-card').next().next();
+	this.bitCoinInfo.hide();
 }
 
 PaymentInfo.prototype.onchangeHandler = function() {
-	var infoSib = document.getElementsByClassName('info');
 		if(this.paymentInfoMenu.value === 'paypal') {
 			$('#credit-card').hide('slow');
-			$(infoSib[0]).show('slow');
-			$(infoSib[1]).hide('slow');
-			$('#payment').next().hide('slow');
+			this.paypalInfo.show('slow');
+			this.bitCoinInfo.hide('slow');
 		} else if (this.paymentInfoMenu.value === 'credit card') {
 			$('#credit-card').show('slow');
-			$(infoSib[0]).hide('slow');
-			$(infoSib[1]).hide('slow');
-			$('#payment').next().hide('slow');
+			this.paypalInfo.hide('slow');
+			this.bitCoinInfo.hide('slow');
 		} else if(this.paymentInfoMenu.value === 'bitcoin') {
 			$('#credit-card').hide('slow');
-			$(infoSib[0]).hide('slow');
-			$(infoSib[1]).show('slow');
-			$('#payment').next().hide('slow');
-		} else if(this.paymentInfoMenu.value === 'select_method') {
-			$('#credit-card, infoSib[0], infoSib[1]').hide('slow');
-			$('#payment').next().show('slow');
+			this.paypalInfo.hide('slow');
+			this.bitCoinInfo.show('slow');
 		}
 };
 
 var newPaymentInfo = new PaymentInfo();
 
-document.getElementById('payment').addEventListener('change', function () {
+document.getElementById('payment').addEventListener('change', (e) => {
 	newPaymentInfo.onchangeHandler();
 })
 
-//6. --FORM VALIDATION--
+/* -----FORM VALIDATION----- */
+
 function FormValidator() {
 	this.nameField = document.getElementById('name');
 	this.emailField = document.getElementById('mail');
@@ -209,89 +204,107 @@ function FormValidator() {
 	this.zip = document.getElementById('zip');
 	this.cvv = document.getElementById('cvv');
 	this.emailPattern = /(\w+)@(\w+)\.(\w+)/;
-	$('#mail').next().hide();
+	this.isNumberPattern = /^\d+$/;
+
+	$('input[type=text], input[type=email]').each(function(index) {
+		const fieldName = $(this).prev().text();
+		$(this).before('<p style="color: red">Please enter the ' + fieldName + '</p>');
+		$(this).prev().hide();
+	});
+
+	$('#total').after('<p style="color: red">Please select at least one activity</p>');
+	$('#total').next().hide();
 }
 
-FormValidator.prototype.textValidator = function () {
-			if (this.value === '') {
-				$(this).next().show('slow');
-				$(this).css('border-color', 'firebrick');
-			} else {
-				$(this).next().hide('slow');
-				$(this).css('border-color', 'green');
-			};
-		}
-
-FormValidator.prototype.textFieldValidator = function() {
-	$('input[type=text]').each(function (i, el) {
-		$(this).keyup(newFormValidator.textValidator);
-	});
+FormValidator.prototype.isFieldEmpty = function (selector) {
+	return $(selector).val() === '';
 };
 
-FormValidator.prototype.emailFieldValidator = function() {
-	if(!this.emailPattern.test(this.emailField.value)) {
-		$('#mail').next().show('slow');
-		$('#mail').css('border-color', 'firebrick');
+FormValidator.prototype.isEmailValid = function () {
+	return this.emailPattern.test(this.emailField.value);
+};
+
+FormValidator.prototype.isCardNumberValid = function () {
+	return this.isNumberPattern.test(this.ccNumber.value) && this.ccNumber.value.length >= 13 && this.ccNumber.value.length <= 16;
+};
+
+FormValidator.prototype.isZipValid = function () {
+	return this.isNumberPattern.test(this.zip.value) && this.zip.value.length === 5;
+};
+
+FormValidator.prototype.isCvvValid = function () {
+	return this.isNumberPattern.test(this.cvv.value) && this.cvv.value.length === 3;
+};
+
+FormValidator.prototype.textFieldEmptyEvent = function (selector) {
+	if(this.isFieldEmpty(selector)) {
+		$(selector).prev().show('slow');
+		$(selector).css('border-color', 'red');
 	} else {
-		$('#mail').next().hide('slow');
-		$('#mail').css('border-color', 'green');
-	};
+		$(selector).prev().hide('slow');
+		$(selector).css('border-color', 'green');
+	}
+};
+
+FormValidator.prototype.showingErrorMessageCondition = function (condition, selector) {
+	if (condition) {
+		$(selector).prev().show('slow');
+		$(selector).css('border-color', 'red');
+	} else {
+		$(selector).prev().hide('slow');
+		$(selector).css('border-color', 'green');
+	}
+};
+
+FormValidator.prototype.emailConfirmationEvent = function () {
+	this.showingErrorMessageCondition(!this.isEmailValid(), '#mail');
+};
+
+FormValidator.prototype.creditCardConfirmationEvent = function () {
+	this.showingErrorMessageCondition(!this.isCardNumberValid(), '#cc-num');
+};
+
+FormValidator.prototype.zipConfirmationEvent = function () {
+	this.showingErrorMessageCondition(!this.isZipValid(), '#zip');
+};
+
+FormValidator.prototype.cvvConfirmationEvent = function () {
+	this.showingErrorMessageCondition(!this.isCvvValid(), '#cvv');
 };
 
 FormValidator.prototype.activitiesValidator = function() {
-	$('#total').next().hide();
 	$('.activities label>input').each(function(e, el) {
-		el.onchange = function () {
+		el.addEventListener('change', (e) => {
 			if (el.checked) {
-				$('#total').next().hide();
+				$('#total').next().hide('slow');
 			} else {
-				$('#total').next().show();
+				$('#total').next().show('slow');
 			};
-		}
+		})
 	});
 }
 
-FormValidator.prototype.submitButtonClickHandler = function() {
-	this.textFieldValidator();
-};
-
 var newFormValidator = new FormValidator();
 
-newFormValidator.textFieldValidator();
-
-$('#mail').keyup(function(){
-	newFormValidator.emailFieldValidator()
-});
+$('#name').focus(() => {newFormValidator.textFieldEmptyEvent('#name')}).keyup(() => {newFormValidator.textFieldEmptyEvent('#name')});
+$('#other-title').focus(() => {newFormValidator.textFieldEmptyEvent('#other-title')}).keyup(() => {newFormValidator.textFieldEmptyEvent('#other-title')});
+$('#mail').focus(() => {newFormValidator.emailConfirmationEvent()}).keyup(() => {newFormValidator.emailConfirmationEvent()});
+$('#cc-num').focus(() => {newFormValidator.creditCardConfirmationEvent()}).keyup(() => {newFormValidator.creditCardConfirmationEvent()});
+$('#zip').focus(() => {newFormValidator.zipConfirmationEvent()}).keyup(() => {newFormValidator.zipConfirmationEvent()});
+$('#cvv').focus(() => {newFormValidator.cvvConfirmationEvent()}).keyup(() => {newFormValidator.cvvConfirmationEvent()});
 
 newFormValidator.activitiesValidator();
 
-$('input').focusout(function () {
-	if($(this).val() === '') {
-		$(this).next().show('slow');
-		$(this).css('border-color', 'firebrick');
-	}
-});
-
-$('form').submit(function (event) {
-	event.preventDefault();
-	if ($('#payment').val() === 'select_method') {
-		$('#payment').next().show('slow');
-	};
-	$('input').each(function (i, el) {
-		if ($(el).val() === '') {
-			$(this).next().show('slow');
-			$(this).css('border-color', 'firebrick');
-		}
-
-	});
-})
-
-	//a) Name field can't be empty
-	//b) Email field must be valid--regex
-	//c) At least one activity must be checked
-	//d) Payment option must be selected
-	//e) Fields in the credit card must be filled
-//7. --EXTRA CREDIT--
-	//a) Hide the Color label and select field until t-shirt design is selected
-	//b) style select menus like the rest of input fields
-	//c) Validate the credit card number
+// $('form').submit(function (event) {
+// 	event.preventDefault();
+// 	if ($('#payment').val() === 'select_method') {
+// 		$('#payment').next().show('slow');
+// 	};
+// 	$('input').each(function (i, el) {
+// 		if ($(el).val() === '') {
+// 			$(this).next().show('slow');
+// 			$(this).css('border-color', 'firebrick');
+// 		}
+//
+// 	});
+// })
